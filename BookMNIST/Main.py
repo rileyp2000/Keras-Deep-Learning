24b6c60fd978ef2a1a8acc42fe5c46d054318086
rileyp2000 <rileyp@bishopireton.org>
2019-03-15 10:01:30 -0400

    finished first NN, working on optimization

---
+++ BookMNIST/Main.py
@@ -0,0 +1,7 @@
+#Patrick Riley
+#3/15/19
+
+import numpy as np
+import mnist_loader
+
+trainingData, validationData, testData = mnist_loader.load_data_wrapper()
--- BookMNIST/Network.py
+++ BookMNIST/Network.py
@@ -65,7 +65,59 @@ class Network(object):
         self.weights = [w-(eta/len(miniBatch))*nw for w, nw in zip(self.weights, nabla_w)]
         self.biases = [b-(eta/len(miniBatch))*nb for b,nb in zip(self.biases, nabla_b)]
 
+    def backprop(self, x, y):
+        """Return a tuple ``(nabla_b, nabla_w)`` representing the
+                gradient for the cost function C_x.  ``nabla_b`` and
+                ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
+                to ``self.biases`` and ``self.weights``."""
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+
+        #Feedforward
+        activation = x
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
+        for b,w in zip(self.biases,self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+        #backward pass
+        delta = self.costDerivative(activations[-1],y) * \
+            sigmoidPrime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta,activations[-2].transpose())
+        # l = 1 means the last layer of neurons, l = 2 is the
+        # second-last layer, and so on.  It's a renumbering of the
+        # scheme in the book, used here to take advantage of the fact
+        # that Python can use negative indices in lists.
+        for l in range(2, self.numLayers):
+            z = zs[-1]
+            sp = sigmoidPrime(z)
+            delta = np.dot(self.weights[-l+1].transpose(),delta)*sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        return (nabla_b, nabla_w)
+
+    def evaluate(self, testData):
+        """Return the number of test inputs for which the neural
+                network outputs the correct result. Note that the neural
+                network's output is assumed to be the index of whichever
+                neuron in the final layer has the highest activation."""
+        testResults = [(np.argmax(self.feedforward(x)),y) for x,y in testData]
+        return sum(int(x == y) for x,y in testResults)
+
+    def costDerivative(self, outputActivations, y):
+        """Return the vector of partial derivatives \partial C_x /
+                \partial a for the output activations."""
+        return (outputActivations - y)
+
+
 
 
 def sigmoid(z):
     return 1.0/(1.0 + np.exp(-z))
+
+def sigmoidPrime(z):
+    """Derivative of the sigmoid function."""
+    return sigmoid(z)*(1-sigmoid(z))
